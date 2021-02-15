c6a42f65f79d1f76505b1f4b19e4e8bdad7a7ba8
ilarioni <ilia.meshvildishvili@gmail.com>
2021-02-15 19:30:45 +0100

    module

--- angularJS02.js
+++ angularJS02.js
@@ -5,3 +5,17 @@
 // <div ng-app="myApp">...</div>
 // <script>"belows text here"</script>
 var app = angular.module("myApp", []);
+// adding controller to application and refer to the controller with the ng-controller
+/*
+<div ng-app="myApp" ng-controller="myCtrl">
+{{ firstName + " " + lastName }}
+</div>
+
+<script>
+var app = angular.module("myApp", []);
+app.controller("myCtrl", function($scope) {
+    $scope.firstName = "John";
+    $scope.lastName = "Doe";
+});
+</script>
+*/
