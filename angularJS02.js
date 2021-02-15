b1cd0b3c9edc467b257bd0c505d751c3c3f05b05
ilarioni <ilia.meshvildishvili@gmail.com>
2021-02-15 19:30:00 +0100

    initial setup

---
+++ angularJS02.js
@@ -0,0 +1,7 @@
+// AngularJS Modules
+
+// Angular JS module defines an application. module is a container for the application controllers. controllers always belong to a module.
+
+// <div ng-app="myApp">...</div>
+// <script>"belows text here"</script>
+var app = angular.module("myApp", []);
