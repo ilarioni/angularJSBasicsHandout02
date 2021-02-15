7241be1135dce07eaaed2605177e02a5f9ccdb88
ilarioni <ilia.meshvildishvili@gmail.com>
2021-02-15 19:31:24 +0100

    directive

--- angularJS02.js
+++ angularJS02.js
@@ -19,3 +19,17 @@ app.controller("myCtrl", function($scope) {
 });
 </script>
 */
+
+/*
+<div ng-app="myApp" w3-test-directive></div>
+
+<script>
+var ap
+p = angular.module("myApp", []);
+app.directive("w3TestDirective", function() {
+    return {
+        template : "I was made in a directive constructor!"
+    };
+});
+</script>
+*/ 
