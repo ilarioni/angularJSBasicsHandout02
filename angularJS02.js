cc3740fdbdac04fdfa996aa0d254847904cfcc51
ilarioni <ilia.meshvildishvili@gmail.com>
2021-02-15 19:32:31 +0100

    top to bottom

--- angularJS02.js
+++ angularJS02.js
@@ -33,3 +33,40 @@ app.directive("w3TestDirective", function() {
 });
 </script>
 */ 
+
+// In this example, "myApp.js" contains an application module definition, while "myCtrl.js" contains the controller:
+
+/*
+<div ng-app="myApp" ng-controller="myCtrl">
+{{ firstName + " " + lastName }}
+</div>
+
+<script src="myApp.js"></script>
+<script src="myCtrl.js"></script>
+*/
+
+// While it is common in HTML applications to place scripts at the end of the <body> element, it is recommended that you load the AngularJS library either in the <head> or at the start of the <body>.
+
+// This is because calls to angular.module can only be compiled after the library has been loaded.
+
+/*
+<!DOCTYPE html>
+<html>
+<body>
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
+
+<div ng-app="myApp" ng-controller="myCtrl">
+{{ firstName + " " + lastName }}
+</div>
+
+<script>
+var app = angular.module("myApp", []);
+app.controller("myCtrl", function($scope) {
+  $scope.firstName = "John";
+  $scope.lastName = "Doe";
+});
+</script>
+
+</body>
+</html>
+*/
